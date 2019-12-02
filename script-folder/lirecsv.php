9c0dc73dd22930ec59b58bcb44cfb362bc6cceb6
manogeekmagik <hermannhouedanou@gmail.com>
2019-12-02 11:22:56 +0100

    lecture du csv en php

--- script-folder/lirecsv.php
+++ script-folder/lirecsv.php
@@ -1,35 +1,28 @@
 <?php
 try {
-    $filename = 'bdd-bornes.csv'; // ici j'appele le fichier excel 
+    $filename = 'data/bdd-bornes.csv'; // ici j'appele le fichier excel 
     
     // creation variable du tableau
     $excel_array = []; 
     
     // Open the file for reading
-    if (($h = fopen("{$filename}", "r")) !== FALSE) 
+    if (($handle = fopen("{$filename}" , 'r')) !== FALSE) 
     {
         // Each line in the file is converted into an individual array that we call $data
         // The items of the array are comma separated
-        while (($data = fgetcsv($h, 1000, ",")) !== FALSE) 
+        while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) 
         {
         // Each individual array is being pushed into the nested array
         $excel_array[] = $data;		
         }
 
         // Close the file
-        fclose($h);
+        fclose($handle);
     }
-    
-    // Display the code in a readable format
-    echo "<pre>";
-    //var_dump($excel_array);
-    
-    echo "</pre>";
     
     }
     
-    catch(PDOException $e){
-            echo 'Echec de la connexion : '.$e->getMessage();
-    
+    catch(PDOException $error){
+        echo 'Echec de la connexion : '.$error->getMessage();
     }
 ?>
