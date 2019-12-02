00cd405892882c1f9bba199a987fb34ab65b81cb
mtcha <marie.tcha1809@gmail.com>
2019-12-02 15:28:07 +0100

    20191202 fichiers de connexion

---
+++ Profil/createdatabase.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "dmhma";
+$password = "webornes5";
+$dbname = "webornes";
+
+// Create database +connexion
+try {
+    $connexion = new PDO("mysql:host=$servername;$dbname", $username,$password); 
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $connexion->exec("CREATE DATABASE IF NOT EXISTS webornes CHARACTER SET utf8mb4 COLLATE utf8mb4_bin");
+    
+    
+
+    echo 'Base de données crée'; 
+    }
+    catch(PDOException $e){
+        echo 'Echec de la connexion : '.$e->getMessage();
+    }
