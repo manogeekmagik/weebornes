8a78997be207f0e8e0d8a18839e11565844e9eaf
alex <jelen18@live.fr>
2019-12-03 16:24:07 +0100

    ici le travail du mardi de dimitri et micka sur le front

---
+++ Front/frontwebornes/moncompte.php
@@ -0,0 +1,13 @@
+<?php
+//Doctype HTML
+    include("template/doctype.php");   
+?>
+
+<?php
+//header
+    include("template/header.php");   
+?>
+<?php
+//footer
+    require_once("template/footer.php");   
+?>
