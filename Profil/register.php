80bdd73bd4d278cb484881fc862f2b2b0b77593e
mtcha <marie.tcha1809@gmail.com>
2019-12-09 13:14:22 +0100

    20191209 MAJ Deconnexion

--- Profil/register.php
+++ Profil/register.php
@@ -6,93 +6,65 @@
 
 if(isset($_POST['registration']))
 {       
-    /**
-     * Converts special characters to HTML entities
-     * and secure user data
-     */
-    
+
+/**
+ * Converts special characters to HTML entities
+ * and secure user data
+ */
+
     $pseudo = htmlspecialchars($_POST['pseudo']);
     $mail = htmlspecialchars($_POST['mail']);
     $password = sha1($_POST['password']);
     $password2 = sha1($_POST['password2']);
+    $avatar = $_FILES['avatar']['name'];
+
+
 
     /**
      * Validate user input
      */
 
-    if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['password']) AND !empty($_POST['password2']))
+    if(!empty($_POST['pseudo']) AND !empty($_POST['mail']) AND !empty($_POST['password']) AND !empty($_POST['password2']) AND !empty($_FILES['avatar']))
     {
-        $pseudolength = strlen($pseudo);
-        if($pseudolength <= 255)
-        {
-            if($password == $password2)
-            {
-                $insertusers = $connexion->prepare("INSERT INTO users (pseudo,email, mot_de_passe) VALUES (?, ?, ?)");
-                $insertusers->execute(array($pseudo, $mail, $password));
-                $erreur = 'Votre compte a bien été crée. <a href="index.php">Me connecter</a>';
-                
-            }
-            else
-            {
-                /**
-                * Validate that the password must be equal
-                */
-                $erreur = "Mot de passe différent. Recommencez.";
-            }
-        }
-        else
-        {
-            /**
-            * Length of the nickname
-            */
-            $erreur = "Votre pseudo ne doit pas dépasser 255 caractères.";
-        }
+    $pseudolength = strlen($pseudo);
+
+    if($pseudolength <= 255)
+    {          
+    if($password == $password2)
+    {
+        $insertusers = $connexion->prepare("INSERT INTO users (pseudo,email, mot_de_passe,avatar) VALUES (?, ?, ?, ?)");
+        $insertusers->execute(array($pseudo, $mail, $password, $avatar));
+        $erreur = 'Votre compte a bien été crée. <a href="moncompteutilisateur.php">Me connecter</a>';   /**Modifier ici quand modal effectué */ 
     }
     else
     {
-        /**
-        * Make sure all fields are filled
-        */
-        $erreur = "Tous les champs doivent être complétés.";
+    /**
+    * Validate that the password must be equal
+    */
+    $erreur = "Mot de passe différent. Recommencez.";
     }
 
-    if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name']))
+    }
+    else
     {
-        $tailleMax = 2097152;
-        $extensionValides = array('jpg','jpeg','gif','png');
-        if($_FILES['avatar']['size'] <= $tailleMax)
-        {
-            $extensionUpload = strtolower(substr(strrchr($_FILES['avatar']['name'], '.' ),1));
-            if(in_array($extensionUpload, $extensionValides))
-            {
-                $chemin = "weebornes/avatars/" .$_SESSION['id']. "." . $extensionUpload;
-                $resultat = move_uploaded_file($_FILES ['avatar']['tmp_name'],$chemin);
-                if($resultat)
-                {
-                    $upDateAvatar = $connexion->prepare('UPDATE users SET avatar = :avatar WHERE id = :id');
-                    $upDateAvatar->execute(array(
-                        'avatar' => $_SESSION['id']. "." . $extensionUpload,
-                        'id'=> $_SESSION['id']
-
-                    ));
-                }
-                else
-                {
-                    $msg = "Erreur de téléchargement.";
-                }
-            }
-            else
-            {
-                $msg = "Votre photo de profil doit être au format JPG, JPEG, GIF et PNG.";
-            }
-        }
-        else
-        {
-            $msg = "Votre photo de profil ne doit pas dépasser 2MO.";
-        }
+    /**
+    * Length of the nickname
+    */
+    $erreur = "Votre pseudo ne doit pas dépasser 255 caractères.";
     }
+    }
+    else
+    {
+    /**
+    * Make sure all fields are filled
+    */
+    $erreur = "Tous les champs doivent être complétés.";
+    }
+
+
 
 }
+
 
 
 
