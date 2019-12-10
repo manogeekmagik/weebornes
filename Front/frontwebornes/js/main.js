14f75d95230c8c3d219c410036dce1107044a7ef
manogeekmagik <hermannhouedanou@gmail.com>
2019-12-10 11:07:33 +0100

    UP2

--- Front/frontwebornes/js/main.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#Bouton").onclick = function() {
-    if (window.getComputedStyle(document.querySelector('#infoborne')).display=='none'){
-    document.querySelector("#infoborne").style.display="block";
-    } else { 
-    document.querySelector("#infoborne").style.display="none";
-    }
-    }
