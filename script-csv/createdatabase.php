51d98127d5f1c28715ad5f709de6fd41641270ed
manogeekmagik <hermannhouedanou@gmail.com>
2019-12-10 11:13:57 +0100

    createdatabase up

--- script-csv/createdatabase.php
+++ script-csv/createdatabase.php
@@ -11,11 +11,7 @@ try {
 
     $connexion->exec("CREATE DATABASE IF NOT EXISTS webornes CHARACTER SET utf8mb4 COLLATE utf8mb4_bin");
 
-<<<<<<< HEAD
     /* echo 'Base de données crée'; */ 
-=======
-    echo 'Base de données crée'; 
->>>>>>> cd9fec3a3e8967bd1dfa31e3f768987d38608fe7
     }
     catch(PDOException $e){
     echo 'Echec de la connexion : '.$e->getMessage();
